ef6993665cfaab1c01eb01e793e7433ffa73dc68
brygulley25 <brygulley25@gmail.com>
2023-02-22 17:13:13 -0800

    update

--- src/data.js
+++ src/data.js
@@ -22,6 +22,22 @@ export const projects = [
       "this simple React project, are typically defined in JavaScript files and rendered to the DOM using JSX. This single page project makes use of props and state to manage the data displayed on the page",
     link: "https://frontend-bootcamp3.onrender.com/",
     image: "./frontend bootcamp.gif",
+  },
+  {
+    title: "Gulley Wine Project ",
+    subtitle: "UX/UI design to HTML & CSS",
+    description:
+      "From a Figma design to a live website, our project seamlessly transitioned from concept to reality. The Figma designs provided the blueprint for the website's layout, typography, and visual style. eCommerce project",
+    link: "https://statuesque-semifreddo-bcdae1.netlify.app/home",
+    image: "./FigmatoHTMl.gif",
+  },
+    {
+    title: "Divine Design  ",
+    subtitle: "Company site made with React",
+    description:
+      "Welcome to our company site! Here, you can easily get a free quote and learn everything you need to know about our brand. Our user-friendly interface allows you to access all the information you need in just a few clicks.",
+    link: "https://www.divinedesignllc.art/",
+    image: "./DivineDesign.gif",
   }
 ];
 
@@ -31,6 +47,9 @@ export const skills = [
   "React",
   "HTML",
   "CSS",
+  "Figma",
+  "WordPress",
+  "MERN Stack"
   
   
 ];
